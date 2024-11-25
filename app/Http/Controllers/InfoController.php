150dfe34e626e85240d6781e8c48ab6bae1aeda0
bozhevik <danilkasoyka@gmail.com>
2024-11-25 17:42:39 +0100

    Added routes and InfoController

---
+++ app/Http/Controllers/InfoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InfoController extends Controller
+{
+    public function serverInfo()
+    {
+        return response()->json(['php_version' => phpversion()]);
+    }
+
+    public function clientInfo(Request $request)
+    {
+        return response()->json([
+            'ip' => $request->ip(),
+            'user_agent' => $request->header('User-Agent'),
+        ]);
+    }
+
+    public function databaseInfo()
+    {
+        return response()->json([
+            'connection' => config('database.default'),
+        ]);
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InfoController;
 
+// Главная страница
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Проверка локализации
+Route::get('/check-locale', function () {
+    return app()->getLocale();
+});
+
+// Информационные маршруты
+Route::prefix('info')->group(function () {
+    Route::get('/server', [InfoController::class, 'serverInfo']);
+    Route::get('/client', [InfoController::class, 'clientInfo']);
+    Route::get('/database', [InfoController::class, 'databaseInfo']);
+});
+
